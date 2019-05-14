659734be1039a47a3fbdd3bf87a695a350747a55
jives <1925184+jives@users.noreply.github.com>
2019-05-14 20:26:34 +0200

    Updated custom KBD67v2 layout; moved Fn and introduced RCTL

--- keyboards/kbdfans/kbd67/rev2/keymaps/jives/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/jives/keymap.c
@@ -18,23 +18,23 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
-   * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Home|
+   * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Del |
    * |----------------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |End |
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |PgUp|
    * |----------------------------------------------------------------|
-   * |Ctrl   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+   * |Ctrl   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgDn|
    * |----------------------------------------------------------------|
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
+   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|Fn  |
    * |----------------------------------------------------------------|
-   * |Ctrl|Win |Alt |        Space          |Alt| FN|    |Lef|Dow|Rig |
+   * |Ctrl|Win |Alt |        Space          |Alt |Ctrl|  |Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
 [0] = LAYOUT_65_ansi(
-  KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, \
+  KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
   KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,  \
   KC_LCTL, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
-  KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGUP, \
-  KC_LCTL, KC_LGUI, KC_LALT,                LT(1, KC_SPC),                   KC_RALT, MO(2),   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   MO(2),   \
+  KC_LCTL, KC_LGUI, KC_LALT,                LT(1, KC_SPC),                   KC_RALT, KC_RCTL, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap Space Layer
    * ,----------------------------------------------------------------.
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |      |Lef|Dow|Rig|   |   |Lef|Dow|Up |Rig|   |   |        |PSc |
    * |----------------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |   |   |   |      |PUp|SLk |
+   * |        |   |   |   |   |   |   |   |   |   |   |      |PUp|    |
    * |----------------------------------------------------------------|
    * |    |    |    |                       |   |   |    |Hom|PDn|End |
    * `----------------------------------------------------------------'
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,  \
    KC_CAPS, _______, KC_UP,   _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______, _______, KC_PAUS, \
    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, KC_PSCR, \
-   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_PGUP, KC_SLCK, \
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_PGUP, _______, \
    _______, _______, _______,                   _______,                            _______, _______, _______, KC_HOME, KC_PGDN, KC_END),
 
   /* Keymap Fn Layer
